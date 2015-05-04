82530595216202a483fabead5df682237c679858
thforax <thibault.forax@futurasoft.net>
2015-05-04 18:35:26 +0200

    Change on File class
    Add methods

--- file/class.file.php
+++ file/class.file.php
@@ -26,24 +26,44 @@ class Pabana_File {
 		return is_file($this->sFilePath);
 	}
 	
+	public function getAbsolutePath() {
+		return realpath($this->sFilePath);
+	}
+	
+	public function getContent() {
+		return file_get_contents($this->sFilePath);
+	}
+	
+	public function getExtension() {
+		return pathinfo($this->sFilePath, PATHINFO_EXTENSION);
+	}
+	
 	public function import() {
 		include($this->sFilePath);
 	}
 	
-	public function size() {
-		return filesize($this->sFilePath);
+	public function isDir() {
+		return is_dir($this->sFilePath);
+	}
+	
+	public function isFile() {
+		return is_file($this->sFilePath);
+	}
+	
+	public function isLink() {
+		return is_link($this->sFilePath);
+	}
+	
+	public function isUpload() {
+		return is_uploaded_file($this->sFilePath);
 	}
 	
 	public function name() {
 		return basename($this->sFilePath);
 	}
 	
-	public function getAbsolutePath() {
-		return realpath($this->sFilePath);
-	}
-	
-	public function getExtension() {
-		return pathinfo($this->sFilePath, PATHINFO_EXTENSION);
+	public function size() {
+		return filesize($this->sFilePath);
 	}
 }
 ?>
