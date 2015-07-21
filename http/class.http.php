09eed7605e0be0e64ccb20f3b7a69744afe75970
thforax <thibault.forax@futurasoft.net>
2015-07-21 16:34:15 +0200

    Add isAjax method
    Add isAjax method to know if current request is ajax

--- http/class.http.php
+++ http/class.http.php
@@ -7,5 +7,13 @@ class Pabana_Http {
 		}
 		return $this;
 	}
+	
+	public function isAjax() {
+		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
 ?>
